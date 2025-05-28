04f59f2b63c9c61db8ab77c56c73cffa8654caae
OctoSpark <jeanpaul.hero2@gmail.com>
2025-05-28 10:00:51 -0500

    Med refund

--- src/views/form/LifeChanges.jsx
+++ src/views/form/LifeChanges.jsx
@@ -11,41 +11,19 @@ const all = [client, ...dependents].filter(x => x['cover'] == 'true' || x['cover
 export default function LifeChanges() {
 
   const [disabled, setDisabled] = useState(false)
-  const [required, setRequired] = useState(false)
-  const [params] = useSearchParams()
+  const [required, setRequired] = useState(true)
 
-  const k = parseInt(params.get('k')) || 0
 
   const goTo = e => {
     e.preventDefault()
 
-    const data = new FormData(e.target)
-    all[k]['life-change'] = data.getAll('life-change')
-
-    all.forEach((x, i) => {
-
-      if (x == client) {
-
-        setSessionToJSON('client', x)
-      } else {
-
-        dependents.forEach((dep, index) => {
-          if (x == dep) {
-            dependents[index] = x
-          }
-        })
-        setSessionToJSON('dependents', dependents)
-
-      }
-
-    })
-
-    if ((k + 1) < all.length) {
-      location.href = `/life-changes?k=${k + 1}`
-    } else {
-      location.href = 'final-section-welcome'
-
+    if (required) {
+      alert('Debes seleccionar uno')
+      return
     }
+
+    location.href = 'final-section-welcome'
+
 
 
   }
@@ -55,37 +33,34 @@ export default function LifeChanges() {
 
       <h2 className="section-title">D&iacute;ganos sobre cualquier cambio de vida</h2>
 
-      <p><b>Ha tenido {all[k]['first-name']} cualquiera de estos cambios hace 20/2/{new Date().getFullYear()}</b></p>
+      <p><b>Alguna de estas personas ha tenido cualquiera de estos cambios antes de 20/2/{new Date().getFullYear()}</b></p>
 
       <label htmlFor="life-change-married" className="checkbox-label">
-        <input type="checkbox" name="life-change" id="life-change-married" value={'Casarse'} disabled={disabled} required={required} />
+        <input type="checkbox" name="life-change" id="life-change-married" value={'Casarse'} disabled={disabled} onChange={e => setRequired(!e.target.checked)} />
         <span>Casarse</span>
       </label>
       <label htmlFor="life-change-dependent" className="checkbox-label">
-        <input type="checkbox" name="life-change" id="life-change-dependent" value={'Obtuvo un dependiente (o se convirtió en uno)'} disabled={disabled} required={required} />
+        <input type="checkbox" name="life-change" id="life-change-dependent" value={'Obtuvo un dependiente (o se convirtió en uno)'} disabled={disabled} onChange={e => setRequired(!e.target.checked)} />
         <span>Obtuvo un dependiente (o se convirti&oacute; en dependiente) debido a una adopci&oacute;n, colocaci&oacute;n en un cuidado temporal o de crianza temporal o una orden judicial</span>
       </label>
       <label htmlFor="life-change-moved" className="checkbox-label">
-        <input type="checkbox" name="life-change" id="life-change-moved" value={'Se mudó'} disabled={disabled} required={required} />
+        <input type="checkbox" name="life-change" id="life-change-moved" value={'Se mudó'} disabled={disabled} onChange={e => setRequired(!e.target.checked)} />
         <span>Se mud&oacute;</span>
       </label>
       <label htmlFor="life-change-liberty" className="checkbox-label">
-        <input type="checkbox" name="life-change" id="life-change-liberty" value={'Fue liberado de su encarcelamiento'} disabled={disabled} required={required} />
+        <input type="checkbox" name="life-change" id="life-change-liberty" value={'Fue liberado de su encarcelamiento'} disabled={disabled} onChange={e => setRequired(!e.target.checked)} />
         <span>Fue liberado de su encarcelamiento (detenci&oacute;n o carcel)</span>
       </label>
 
       <hr />
 
       <label htmlFor="life-change-none" className="checkbox-label">
-        <input type="checkbox" name="life-change" id="life-change-none" value={'Ninguno'} onChange={e => setDisabled(e.target.checked)} required={disabled} />
+        <input type="checkbox" name="life-change" id="life-change-none" value={'Ninguno'} onChange={e => {
+          setDisabled(e.target.checked)
+          setRequired(!e.target.checked)
+        }} />
         <span>Ninguno de estos cambios</span>
       </label>
-
-      {
-        required && (
-          <p className='text-red-500'>Necesitas seleccionar al menos una casilla</p>
-        )
-      }
 
       <button type="submit" className="green-btn">Guardar y continuar</button>
 
